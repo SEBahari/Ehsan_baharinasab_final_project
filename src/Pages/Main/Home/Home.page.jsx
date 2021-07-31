76530ca533b26a49128d9abe088356f80a492241
SEBaharinasab <36398369+SEBaharinasab@users.noreply.github.com>
2021-07-31 10:14:14 +0430

    create Sponsers section for HomePage

--- src/Pages/Main/Home/Home.page.jsx
+++ src/Pages/Main/Home/Home.page.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './Home.style.css';
 import Baner from '../../../Components/Main/Baner';
 import Incredible from '../../../Components/Main/Incredible';
+import Sponsers from '../../../Components/Main/Sponsers';
 
 export default function Home() {
    return (
       <>
          <Baner />
          <Incredible bgc={'bg-danger'} />
+         <Sponsers />
+         <Incredible bgc={'bg-success'} />
+         <Sponsers />
       </>
    );
 }
