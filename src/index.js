2babbf71786aeac9bac02197f1be5f11024613ca
SEBaharinasab <36398369+SEBaharinasab@users.noreply.github.com>
2021-08-07 17:21:27 +0430

    Add IRANSans font to project & Add logo.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./fonts/IRANYekanThin.ttf";
+import "./fonts/IRANYekanLight.ttf";
+import "./fonts/IRANYekanRegular.ttf";
+import "./fonts/IRANYekanMedium.ttf";
+import "./fonts/IRANYekanBold.ttf";
+import "./fonts/IRANYekanExtraBold.ttf";
+import "./fonts/IRANYekanBlack.ttf";
+import "./fonts/IRANYekanExtraBlack.ttf";
 import "./index.css";
 
 ReactDOM.render(
