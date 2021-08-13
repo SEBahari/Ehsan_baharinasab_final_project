ab0e2e3cf4b417ffb9d935d57af2d7a73c3c6d39
SEBaharinasab <36398369+SEBaharinasab@users.noreply.github.com>
2021-08-13 12:05:45 +0430

    create CardProduct and use it in homePage

---
+++ src/Components/Main/ProductCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ProductCard({ Image, route, title, subtitle, discount, originalPrice, newPrice }) {
+   return (
+      <div className={"w-72 px-4 inline-block product"}>
+         {/* Card */}
+         <div className="rounded-md bg-white text-gray-800 p-0 overflow-hidden shadow-lg">
+            {/* image */}
+            <div className={"relative h-44 sm:h-32 md:h-40"}>
+               <img className={"absolute inset-0 w-full h-full object-cover object-center"} src={Image} />
+               <Link
+                  className={"absolute inset-0"}
+                  to={route}
+               />
+            </div>
+            {/* content */}
+            <div className={"p-4 bg-gradient-to-b from-yellow-50 to-gray-200 "}>
+               <div className={"flex items-end border-b border-gray-300 pb-2 mb-3"}>
+                  <div className={"title flex-1 relative"}>
+                     <h4 className={"text-xl"}>{title}</h4>
+                     <h5 className={"text-md"}>{subtitle}</h5>
+                     <Link
+                        className={"absolute inset-0"}
+                        to={route}
+                     />
+                  </div>
+                  <div className="colors flex pb-1">
+                     <span className="w-3 h-3 rounded-full bg-purple-800 mr-2" />
+                     <span className="w-3 h-3 rounded-full bg-yellow-400 mr-2" />
+                     <span className="w-3 h-3 rounded-full bg-pink-600 mr-2" />
+                  </div>
+               </div>
+               <div className={"w-full flex items-end"}>
+                  <button><i className={"bi bi-cart3 text-3xl pl-3 md:text-2xl"} /></button>
+                  <button>
+                     <i className={"bi bi-heart text-3xl md:text-2xl"} />
+                     {/* <i className={"bi bi-heart-fill text-red-600 text-2xl md:text-xl"} /> */}
+                  </button>
+                  <div className="price flex flex-col flex-1 text-left">
+                     <div className="befPrice flex flex-row-reverse items-end">
+                        <span className={"bg-red-600 rounded-3xl text-red-50 p-1 text-xs"}>{discount}%</span>
+                        <span className="prPrice text-sm line-through pl-2">{originalPrice}$</span>
+                     </div>
+                     <div className={"pl-1 text-xl"}>{newPrice}$</div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default ProductCard;
--- src/Pages/Main/Home/Home.page.jsx
+++ src/Pages/Main/Home/Home.page.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Home.style.css";
-import bannerImage
-  from "../../../images/6x6-gornos-w.jpg";
 import Banner from "../../../Components/Main/Banner";
+import bannerImage from "../../../images/00-mercedes-benz-vision-eq-silver-arrow-world-premiere-monterey-car-week-2018-3400x1440.jpg";
+import cardImage1 from "../../../images/6x6-gornos-w.jpg";
+import cardImage2 from "../../../images/photo_۲۰۱۷-۱۲-۲۸_۱۱-۵۹-۱۹.jpg";
+import cardImage3 from "../../../images/AAFW1413.png";
+import cardImage4 from "../../../images/ford_mustang_muscle_car-wide.jpg";
+import cardImage5 from "../../../images/photo_2017-09-07_10-40-17.jpg";
+import { Link } from "react-router-dom";
+import ProductCard from "../../../Components/Main/ProductCard";
 
 export default function Home() {
   return (
     <>
+      {/*Banner*/}
       <Banner
         image={bannerImage}
         alignment={"left"}
@@ -16,6 +23,77 @@ export default function Home() {
         </>}
         route={"/products/mercedes-g-500"}
       />
+
+      {/* incredible products */}
+      <div className={"py-4"}>
+        {/* title & link */}
+        <div className="flex items-center justify-between p-3 pt-6">
+          <h3 className={"text-lg"} >تخفیفات ویژه</h3>
+          <Link className={"underline text-sm"} to={'/products?category=incredible'} >
+            مشاهده همه
+            <i className={"bi bi-chevron-compact-left"} />
+          </Link>
+        </div>
+        {/* products list */}
+        <div className="overflow-x-auto overflow-y-hidden whitespace-nowrap">
+
+          {/* product 1 */}
+          <ProductCard
+            Image={cardImage1}
+            title={"مرسدس بنز"}
+            subtitle={"G-500"}
+            discount={10}
+            originalPrice={'100,000'}
+            newPrice={'90,000'}
+            route={'/products/mercedes'}
+          />
+
+
+          {/* product 2 */}
+          <ProductCard
+            Image={cardImage2}
+            title={"تویوتا"}
+            subtitle={"GTR"}
+            discount={10}
+            originalPrice={"100,000"}
+            newPrice={"90,000"}
+            route={"/products/gtr"}
+          />
+
+          {/* product 3 */}
+          <ProductCard
+            Image={cardImage3}
+            title={"دوج چلنجر"}
+            subtitle={"Helcat"}
+            discount={10}
+            originalPrice={"100,000"}
+            newPrice={"90,000"}
+            route={"/products/chalenger"}
+          />
+
+          {/* product 4 */}
+          <ProductCard
+            Image={cardImage4}
+            title={"فورد"}
+            subtitle={"mustang"}
+            discount={10}
+            originalPrice={"100,000"}
+            newPrice={"90,000"}
+            route={"/products/mustang"}
+          />
+
+          {/* product 5 */}
+          <ProductCard
+            Image={cardImage5}
+            title={"پورشه"}
+            subtitle={"macan"}
+            discount={10}
+            originalPrice={"100,000"}
+            newPrice={"90,000"}
+            route={"/products/macan"}
+          />
+        </div>
+      </div>
     </>
   );
 }
