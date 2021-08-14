092d86979d46724b911d627b0860c855e11a0932
SEBaharinasab <36398369+SEBaharinasab@users.noreply.github.com>
2021-08-14 22:43:22 +0430

    create productsPage for mobile size and rotated mobile size

--- src/Pages/Main/Products/Products.page.jsx
+++ src/Pages/Main/Products/Products.page.jsx
@@ -1,148 +1,73 @@
 import React from "react";
-import { Row, Col, Container, ListGroup, Card, Button } from "react-bootstrap";
+import HProduct from "../../../Components/Main/HProduct";
+import cardImage1 from '../../../images/01-Mercedes-Benz-G-Class-G-500-4x4-2-Wallpaper-3840x2160.jpg';
+import cardImage2 from '../../../images/6x6-gornos-w.jpg';
+import cardImage3 from '../../../images/challenger1.jpeg';
+import cardImage4 from '../../../images/photo_۲۰۱۷-۱۲-۲۸_۱۱-۵۹-۱۹.jpg';
+import cardImage5 from '../../../images/porsche-911-rsr-2880x1800-2017-side-view-4019.jpg';
+
+const list = [
+  { image: cardImage1, title: 'G-Class', subtitle: "G-500 4x4", seller: "Mercedes Benz", rate: "4.9", rateOf: "378", discount: "13", originalPrice: "100,000", newPrice: "90,000" },
+  { image: cardImage2, title: 'G-Class', subtitle: "G-63 6x6", seller: "Mercedes Benz", rate: "5", rateOf: "40", originalPrice: "1,100,000" },
+  { image: cardImage3, title: 'Challenger', subtitle: "SRT", seller: "Dodge", rate: "3.9", rateOf: "834", originalPrice: "400,000" },
+  { image: cardImage4, title: 'Nissan', subtitle: "GTR", seller: "IranKhodro", rate: "5", rateOf: "78", discount: "3", originalPrice: "100,000", newPrice: "90,000" },
+  { image: cardImage5, title: 'Porsche', subtitle: "911 RSR", seller: "Saipa Motmaen", rate: "4.9", rateOf: "378", discount: "13", originalPrice: "100,000", newPrice: "90,000" },
+];
 
 function Products() {
   return (
-    <Container fluid>
-      <Row className={"g-0 mt-3"}>
-        <Col xs={3} className={"pe-4"}>
-          <ListGroup className={"shadow-sm rounded-3"}>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col xs={9}>
-          <Row xs={1} sm={2} md={4} className={"g-4 justify-content-around"}>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className={"mb-2"}>
-              <Card>
-                <Card.Img
-                  variant={"top"}
-                  src={"https://i.pravatar.cc/100?u=" + makeId()}
-                  style={{ height: "8rem", objectFit: "cover", objectPosition: "center" }}
-                />
-                <Card.Body>
-                  <Card.Title>عنوان کالا</Card.Title>
-                  <Card.Text>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+    <div className={"pt-32 container mx-auto sm:pt-36"}>
+      {/* -------------------------------- filters ------------------------------- */}
+      <div className={"flex gap-4 px-4 mb-2 sm:px-10"}>
+        <button className={"rounded-md py-1 px-2 bg-gray-300 text-gray-600"}>
+          فیلتر
+          <i className={"bi bi-funnel pr-2"} />
+        </button>
+        <button className={"rounded-md py-1 px-2 bg-gray-300 text-gray-600"}>
+          پر بازدیدترین
+          <i className={"bi bi-sort-down pr-2"} />
+        </button>
+      </div>
+      <hr className={"mb-1"} />
+
+      {/* ----------------------------- bread-crumbs ----------------------------- */}
+      <div className={"px-4 flex items-end text-sm sm:px-10"}>
+        <span>خانه</span>
+        <i className={"bi bi-chevron-left mx-1 text-xs"} />
+        <span>محصولات</span>
+        <i className={"bi bi-chevron-left mx-1 text-xs"} />
+        <span>here</span>
+      </div>
+      <hr className={"my-1"} />
+
+      {/* ------------------------------- products ------------------------------- */}
+      <div className={"products px-5 py-3 sm:px-10"}>
+        {/* Products List */}
+        <div className={"grid grid-cols-1 gap-6 mt-2"}>
+          {
+            list.map(item => (
+              <HProduct {...item} />
+            ))
+          }
+        </div>
+
+        {/* pagination */}
+        <div className="pagination flex items-baseline justify-center mt-6 mb-4">
+          <div className="last bg-blue-400 w-8 h-8 rounded-full flex items-center justify-center mx-1 sm:w-10 sm:h-10">15</div>
+
+          <span className={"mx-2 sm:mx-3"}>...</span>
+
+          <div className="next bg-blue-400 w-8 h-8 rounded-full flex items-center justify-center mx-1 sm:w-10 sm:h-10">6</div>
+          <div className="cur bg-blue-400 w-8 h-8 rounded-full flex items-center justify-center mx-1 sm:w-10 sm:h-10">5</div>
+          <div className="pre bg-blue-400 w-8 h-8 rounded-full flex items-center justify-center mx-1 sm:w-10 sm:h-10">4</div>
+
+          <span className={"mx-2 sm:mx-3"}>...</span>
+
+          <div className="first bg-blue-400 w-8 h-8 rounded-full flex items-center justify-center mx-1 sm:w-10 sm:h-10">1</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Products;
-
-function makeId() {
-  let result = "";
-  let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-  let charactersLength = characters.length;
-  for ( let i = 0; i < 10; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
